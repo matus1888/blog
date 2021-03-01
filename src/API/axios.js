6c6bfa949c1d0cffd94850e12373484847078331
matus1888 <matus1888@gmail.com>
2021-03-01 12:29:16 +0300

    for deploy of Rock

--- src/API/axios.js
+++ src/API/axios.js
@@ -5,10 +5,10 @@ export default axios.create({
     responseType: "json"
 })
 export let recordsApi=axios.create({
-    baseURL: 'http://localhost:8080/recordings',
+    baseURL: 'http://back.matus.mykeenetic.ru/recordings',
     responseType:"json"
 })
 export let headersApi=axios.create({
-    baseURL: 'http://localhost:8080/headers',
+    baseURL: 'http://back.matus.mykeenetic.ru/headers',
     responseType:"json"
 })
